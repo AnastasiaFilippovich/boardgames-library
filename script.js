c089b346274606e5047b63dbb49b40e4bf30d345
AnastasiaFilippovich <viber2470@gmail.com>
2026-09-02 18:15:56 +0300

    Разметка модального окна карточки

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 newGameModal = document.querySelector('.modal-new');
-modalCloseButton = newGameModal.querySelector('.modal-new__close-button');
+newModalCloseButton = newGameModal.querySelector('.modal-new__close-button');
 newGameButton = document.querySelector('.games-add-new');
+gameCardModal = document.querySelector('.modal-game-card');
+cardModalCloseButton = gameCardModal.querySelector('.modal-game-card__close-button');
+moreInfoButtons = document.querySelectorAll('.game-card__more');
+tabs = gameCardModal.querySelectorAll('.modal-game-card__tab');
+tabButtons = gameCardModal.querySelectorAll('.modal-game-card__tab-button');
 
 const closeNewGameModal = () => {
-    newGameModal.classList.add('hidden');
-    modalCloseButton.removeEventListener('click', closeNewGameModal);
+    newGameModal.classList.remove('modal-new--active');
+    newModalCloseButton.removeEventListener('click', closeNewGameModal);
 }
 
 const openNewGameModal = () => {
-    newGameModal.classList.remove('hidden');
-    modalCloseButton.addEventListener('click', closeNewGameModal);
+    newGameModal.classList.add('modal-new--active');
+    newModalCloseButton.addEventListener('click', closeNewGameModal);
+}
+
+const closeGameCardModal = () => {
+    gameCardModal.classList.remove('modal-game-card--active');
+    cardModalCloseButton.removeEventListener('click', closeGameCardModal);
+}
+
+const openGameCardModal = () => {
+    gameCardModal.classList.add('modal-game-card--active');
+    cardModalCloseButton.addEventListener('click', closeGameCardModal);
 }
 
 newGameButton.addEventListener('click', openNewGameModal);
+moreInfoButtons.forEach(element => {
+    element.addEventListener('click', openGameCardModal);
+});
+
+tabButtons.forEach(tabButton => {
+    tabButton.addEventListener('click', () => {
+        tabButtons.forEach(tabButton => {
+            tabButton.classList.remove('.tab-button--active');
+        });
+        tabs.forEach(tab => {
+            tab.classList.remove('.tab--active');
+        });
+
+        tabButton.classList.add('.tab-button--active');
+    });
+});
