7d5e5a7c61ba6379bcd7d92211521d30774d30f9
AnastasiaFilippovich <viber2470@gmail.com>
2026-09-03 21:01:15 +0300

    Открытие большой карточки, добавление игр

--- script.js
+++ script.js
@@ -59,8 +59,12 @@ const TAGS = {
     magic: 'Магическая',
     adventure: 'Приключенческая',
     strategic: 'Стратегическая',
-    cooperative: 'Командая'
+    cooperative: 'Командная',
+    chaotic: 'Хаотичная',
+    competitive: 'Соревновательная'
 };
+
+let nextId = GAMES.length + 1;
 
 const newGameModal = document.querySelector('.modal-new');
 const newModalCloseButton = newGameModal.querySelector('.modal-new__close-button');
@@ -74,29 +78,57 @@ const newGameForm = document.querySelector('.modal-form');
 const gameCardsBlock = document.querySelector('.games-list');
 const gameCardTemplate = document.querySelector('#game-card-template');
 
-const renderGameCards = (gamesData) => {
-  gameCardsBlock.querySelectorAll('.game-card').forEach((element) => element.remove());
+const gameName = gameCardModal.querySelector('.modal-game-card__title');
+const gamePlayers = gameCardModal.querySelector('.modal-game-card__players');
+const gameTime = gameCardModal.querySelector('.modal-game-card__time');
+const gameRating = gameCardModal.querySelector('.modal-game-card__rating');
+const gameDescription = gameCardModal.querySelector('.modal-game-card__description');
+const gameTags = gameCardModal.querySelector('.modal-game-card__tags');
+const modalCloseButton = gameCardModal.querySelector('.modal-game-card__close-button');
+const ratingElementTemplate = document.querySelector('#rating-element-template');
+const notesElementTemplate = document.querySelector('#notes-element-template');
+const ratingContainer = gameCardModal.querySelector('.tab__rating-list');
+const notesContainer = gameCardModal.querySelector('.tab__notes-list');
 
-  const gameCardsBlockFragment = document.createDocumentFragment();
+const createGameCard = (cardData) => {
+    const gameCardsBlockFragment = document.createDocumentFragment();
 
-  gamesData.forEach(({id, name, tags, players, rating}) => {
     const gameCardElement = gameCardTemplate.content.cloneNode(true);
-    gameCardElement.querySelector('.game-card__title').textContent = name;
-    gameCardElement.querySelector('.game-card__players').textContent = players;
-    gameCardElement.querySelector('.game-card__rating').textContent = rating;
-    gameCardElement.querySelector('.game-card').dataset.gameId = id;
+    gameCardElement.querySelector('.game-card__title').textContent = cardData.name;
+    gameCardElement.querySelector('.game-card__players').textContent = cardData.players;
+    gameCardElement.querySelector('.game-card__rating').textContent = cardData.rating;
+    gameCardElement.querySelector('.game-card').dataset.gameId = cardData.id;
 
-    tags.forEach(tag => {
+    cardData.tags.forEach(tag => {
         const tagElement = document.createElement('li');
         tagElement.classList.add('tag');
         tagElement.textContent = TAGS[tag];
         gameCardElement.querySelector('.game-card__tags').append(tagElement);
+    });
+
+    const moreInfoButton = gameCardElement.querySelector('.game-card__more');
+    moreInfoButton.addEventListener('click', (evt) => {
+        const targetGameId = Number(evt.target.closest('.game-card').dataset.gameId);
+
+        if (Number.isNaN(targetGameId)) {
+            return;
+        }
+
+        const targetGameData = GAMES.find((game) => game.id === targetGameId);
+
+        if (targetGameData) {
+            openCardModal(targetGameData);
+        }
     })
 
     gameCardsBlockFragment.appendChild(gameCardElement);
-  });
 
-  gameCardsBlock.appendChild(gameCardsBlockFragment);
+    gameCardsBlock.appendChild(gameCardsBlockFragment);
+};
+
+const renderGameCards = (gamesData) => {
+    gameCardsBlock.querySelectorAll('.game-card').forEach((element) => element.remove());
+    gamesData.forEach(element => createGameCard(element));
 };
 
 const closeNewGameModal = () => {
@@ -108,16 +140,6 @@ const closeNewGameModal = () => {
 const openNewGameModal = () => {
     newGameModal.classList.add('modal-new--active');
     newModalCloseButton.addEventListener('click', closeNewGameModal);
-}
-
-const closeGameCardModal = () => {
-    gameCardModal.classList.remove('modal-game-card--active');
-    cardModalCloseButton.removeEventListener('click', closeGameCardModal);
-}
-
-const openGameCardModal = () => {
-    gameCardModal.classList.add('modal-game-card--active');
-    cardModalCloseButton.addEventListener('click', closeGameCardModal);
 }
 
 newGameButton.addEventListener('click', openNewGameModal);
@@ -141,12 +163,86 @@ tabButtons.forEach(tabButton => {
     });
 });
 
+renderGameCards(GAMES);
+
+const renderRating = (ratingData) => {
+    ratingContainer.querySelectorAll('.rating-list__element').forEach((element) => element.remove());
+
+    const ratingContainerFragment = document.createDocumentFragment();
+
+    ratingData.forEach(({id, author, rating, text, createdAt}) => {
+        const ratingElement = ratingElementTemplate.content.cloneNode(true);
+        ratingElement.querySelector('.element__date').textContent = createdAt;
+        ratingElement.querySelector('.element__author').textContent = author;
+        ratingElement.querySelector('.element__rating').textContent = rating;
+        ratingElement.querySelector('.element__comment').textContent = text;
+        ratingElement.querySelector('.rating-list__element').dataset.ratingId = id;
+
+        ratingContainerFragment.appendChild(ratingElement);
+    });
+
+  ratingContainer.appendChild(ratingContainerFragment);
+};
+
+const renderNotes = (notesData) => {
+    notesContainer.querySelectorAll('.notes-list__element').forEach((element) => element.remove());
+
+    const notesContainerFragment = document.createDocumentFragment();
+
+    notesData.forEach(({id, author, text, createdAt}) => {
+        const notesElement = notesElementTemplate.content.cloneNode(true);
+        notesElement.querySelector('.element__date').textContent = createdAt;
+        notesElement.querySelector('.element__author').textContent = author;
+        notesElement.querySelector('.element__comment').textContent = text;
+        notesElement.querySelector('.notes-list__element').dataset.ratingId = id;
+
+        notesContainerFragment.appendChild(notesElement);
+    });
+
+  notesContainer.appendChild(notesContainerFragment);
+};
+
+const openCardModal = (gameData) => {
+    gameCardModal.classList.add('modal-game-card--active');
+    gameName.textContent = gameData.name;
+    gamePlayers.textContent = gameData.players;
+    gameTime.textContent = gameData.playTime;
+    gameRating.textContent = gameData.rating;
+    gameDescription.textContent = gameData.description;
+    gameTags.innerHTML = '';
+
+    gameData.tags.forEach(tag => {
+        const tagElement = document.createElement('li');
+        tagElement.classList.add('tag');
+        tagElement.textContent = TAGS[tag];
+        gameTags.append(tagElement);
+    });
+
+    if (gameData.reviews) {
+        renderRating(gameData.reviews);
+    }
+
+    if (gameData.notes) {
+        renderNotes(gameData.notes);
+    }
+
+    cardModalCloseButton.addEventListener('click', closeCardModal);
+};
+
+const closeCardModal = () => {
+    gameCardModal.classList.remove('modal-game-card--active');
+    cardModalCloseButton.removeEventListener('click', closeCardModal);
+    ratingContainer.innerHTML = '';
+    notesContainer.innerHTML = '';
+};
+
 newGameForm.addEventListener('submit', function(evt) {
     evt.preventDefault();
 
     const formData = new FormData(newGameForm);
 
     const gameData = {
+        id: nextId,
         name: formData.get('game-name'),
         tags: formData.getAll('tags'),
         players: formData.get('game-players'),
@@ -154,7 +250,8 @@ newGameForm.addEventListener('submit', function(evt) {
         description: formData.get('game-description')
     };
 
-    console.log(gameData);
-});
+    createGameCard(gameData);
 
-renderGameCards(GAMES);
+    GAMES.push(gameData);
+    nextId++;
+});
